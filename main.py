1b6d50f7934787f537b0b099fb8f7dd1c44b5c0d
Luka Guledani <lukaguledani123@gmail.com>
2024-02-17 00:21:20 +0400

    added: post method

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import os 
+import json
 from fastapi import FastAPI, status, HTTPException
 
 
 app = FastAPI()
 
-tasks = [
-    {"id": 1, "Title": "The Dark Knight"},
-    {"id": 2, "Title": "The Watchmen"},
-    {"id": 3, "Title": "The Vendetta"}
-]
+# db folder
+db_folder = "db"
 
+tasks_file = os.path.join(db_folder, "tasks.json")
+
+# Load existing tasks from JSON file if it exists
+if os.path.exists(tasks_file):
+    with open(tasks_file, "r") as file:
+        tasks = json.load(file)
+else:
+    tasks = [
+        {"id": 1, "Title": "The Dark Knight"},
+        {"id": 2, "Title": "The Watchmen"},
+        {"id": 3, "Title": "The Vendetta"}
+    ]
+
+    
+print(tasks)
 @app.get("/")
 def home():
     return tasks
@@ -26,3 +40,12 @@ def v1(task_id: int):
 
     else:
         return result
+
+@app.post("/ty1/{task_id}")
+def create_task(task_id: int,task:dict):
+    new_task_id = max(task["id"] for task in tasks) + 1
+    task["id"] = task_id
+    tasks.append(task)
+    print("Received task:", task)
+    print("Updated tasks list:", tasks)
+    return tasks 
