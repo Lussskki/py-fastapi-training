dc8c60d318d32dd140e7793c93fd87cc4ca4ab29
Luka Guledani <lukaguledani123@gmail.com>
2024-02-17 22:28:50 +0400

    wip: added class and new post method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import json
 import uvicorn
@@ -31,69 +31,44 @@ async def create_request(item:Item):
 if __name__ == "__main__":
             import uvicorn
             uvicorn.run(app, host="0.0.0.0", port=8000)   
-     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import os 
-# import json
-# from fastapi import FastAPI, status, HTTPException
-# app = FastAPI()
-# tasks = [
-#         {"id": 1, "Title": "The Dark Knight"},
-#         {"id": 2, "Title": "The Watchmen"},
-#         {"id": 3, "Title": "The Vendetta"},
-#         {"id": 4, "Title": "Spider man"}
-#         ]
-# # print(tasks)
+"""" 
+if i need this code i will look it, it's for me      
+ import os 
+ import json
+ from fastapi import FastAPI, status, HTTPException
+ app = FastAPI() tasks = [
+         {"id": 1, "Title": "The Dark Knight"},
+         {"id": 2, "Title": "The Watchmen"},
+         {"id": 3, "Title": "The Vendetta"},
+         {"id": 4, "Title": "Spider man"}
+         ]
+ # print(tasks)
 
     
-# @app.get("/")
-# def home():
-#     return tasks
+ @app.get("/")
+ def home():
+     return tasks
 
-# @app.get("/ty1/{task_id}")
-# def v1(task_id: int):
-#     result = {}
-#     for task in tasks:
-#         if task["id"] == task_id:
-#             result = task
-#             return result
+ @app.get("/ty1/{task_id}")
+ def v1(task_id: int):
+     result = {}
+     for task in tasks:
+         if task["id"] == task_id:
+             result = task
+             return result
 
-#     if result == {}:
-#         raise HTTPException(status_code=404, detail=status.HTTP_404_NOT_FOUND)
-#     else:
-#         return result
+     if result == {}:
+         raise HTTPException(status_code=404, detail=status.HTTP_404_NOT_FOUND)
+     else:
+         return result
 
-# @app.post("/ty1/{task_id}")
-# def create_task(task_id: int,task:dict):
-#     new_task_id = max(task["id"] for task in tasks) + 1
-#     task["id"] = task_id
-#     tasks.append(task)
-#     print("Received task:", task)
-#     print("Updated tasks list:", tasks)
-#     return tasks 
+ @app.post("/ty1/{task_id}")
+ def create_task(task_id: int,task:dict):
+     new_task_id = max(task["id"] for task in tasks) + 1
+     task["id"] = task_id
+     tasks.append(task)
+     print("Received task:", task)
+     print("Updated tasks list:", tasks)
+     return tasks 
+"""
