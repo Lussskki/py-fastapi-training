929f85c81e7a431963b46177978573390cc6ff85
Luka Guledani <lukaguledani123@gmail.com>
2024-02-19 21:19:06 +0400

    revrited cods, had errors, configure new item model, added more def functions.

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import json
-import uvicorn
 
 app = FastAPI()
 
-
-
+# Item model
 class Item(BaseModel):
-    product_name: str
-    description: str = None,
-    price: float
-    tex: float = None
+    Quote: str
+    Author: str
 
+# Function to read data from the file
+def read_data_from_file():
+    try:
+        with open("db/database.json", "r", encoding= "utf-8") as json_file:
+            data = json.load(json_file)
+        return data
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError as e:
+        raise HTTPException(status_code=500, detail=f"Error decoding JSON: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading file: {e}")
 
+# Function to write data to the file
+def write_data_to_file(data):
+    try:
+        with open("db/database.json", "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error writing to file: {e}")
 
-# add data in database.json
+# GET method to retrieve data from database.json
+@app.get("/get_request")
+async def get_request():
+    data = read_data_from_file()
+    return JSONResponse(content=data)
+
+# POST method to add data to database.json
 @app.post("/create_request/")
-async def create_request(item:Item):
-                print("Received item data:")
-                print(item.json())
-                try:      
-                    with open("db/database.json", "a") as json_file:
-                            json.dump(item.dict(), json_file, ensure_ascii=False, indent=4)    
-                            json_file.write(",")
-                except Exception as e:
-                        print(f"Error writing to file: {e}")            
-                return item
+async def create_request(item: Item):
+    print("Received item data:")
+    print(item.json())
+    
+    data = read_data_from_file()
+    data.append(item.dict())
+    write_data_to_file(data)
+    
+    return item
 
 if __name__ == "__main__":
-            import uvicorn
-            uvicorn.run(app, host="0.0.0.0", port=8000)   
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
 
 """" 
 if i need this code i will look it, it's for me      
